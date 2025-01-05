97de7914eddc8834b003d63124449fb5c6eaf176
Miyanber <miyanba2984h@gmail.com>
2025-01-05 17:19:25 +0900

    updated

--- src/api/todo_service.js
+++ src/api/todo_service.js
@@ -61,20 +61,14 @@ export class TodoService {
             is_deleted: formData.is_deleted,
         };
 
-        // return TodoService.fetchFromApi(ApiUrls.getTodo).then(data => {
-        //     data.map(item => {
-        //         new Todo(
-        //             item.id,
-        //             item.title, 
-        //             item.detail,
-        //             item.deadLine,
-        //             item.is_done,
-        //             item.is_deleted,
-        //         )
-        //     })
-        // }).catch(error => {
-        //     console.error("Error fetching todos:", error);
-        //     return [];
-        // });
+        return this.fetchFromApi(ApiUrls.manageTodo, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data),
+        }).then(() => true)
+        .catch(error => {
+            console.error("Error updating todo:", error);
+            return false;
+        })
     }
 }
